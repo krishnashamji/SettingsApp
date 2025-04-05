582b0c7c69cd9a83e33dd9230717a7221d0d2a20
Krishna Shamji <shamjidev@gmail.com>
2025-04-05 10:00:04 +0530

    refactor: seperated out constants into their own folder

--- app/index.tsx
+++ app/index.tsx
@@ -2,44 +2,8 @@ import { Text, StyleSheet, View, Image, ScrollView, TouchableOpacity } from 'rea
 import { SFProFontFamily } from '../config/customFont'
 import { FlashList } from "@shopify/flash-list";
 import { useRouter } from 'expo-router';
+import { LISTONEDATA } from "@/constants/constants"
 
-const LISTONEDATA = [
-  {
-    title: "AirPlane Mode",
-    icon: require('../assets/icons/AirPlane_Mode_Icon.png'),
-    page: "/settings/profile"
-  },
-  {
-    title: "Wi-Fi",
-    icon: require('../assets/icons/Wi-Fi_Icon.png'),
-    page: "/settings/profile"
-  },
-  {
-    title: "Bluetooth",
-    icon: require('../assets/icons/Bluetooth_Icon.png'),
-    page: "/settings/profile"
-  },
-  {
-    title: "Cellular",
-    icon: require('../assets/icons/Cellular_Icon.png'),
-    page: "/settings/profile"
-  },
-  {
-    title: "Personal Hotspot",
-    icon: require('../assets/icons/Personal_Hotspot_Icon.png'),
-    page: "/settings/profile"
-  },
-  {
-    title: "Battery",
-    icon: require('../assets/icons/Battery_Icon.png'),
-    page: "/settings/profile"
-  },
-  {
-    title: "VPN",
-    icon: require('../assets/icons/VPN_Icon.png'),
-    page: "/settings/profile"
-  }
-];
 
 export default function Home() {
   const router = useRouter();
---
+++ constants/constants.js
@@ -0,0 +1,37 @@
+export const LISTONEDATA = [
+  {
+    title: "AirPlane Mode",
+    icon: require('../assets/icons/AirPlane_Mode_Icon.png'),
+    page: "/settings/profile"
+  },
+  {
+    title: "Wi-Fi",
+    icon: require('../assets/icons/Wi-Fi_Icon.png'),
+    page: "/settings/profile"
+  },
+  {
+    title: "Bluetooth",
+    icon: require('../assets/icons/Bluetooth_Icon.png'),
+    page: "/settings/profile"
+  },
+  {
+    title: "Cellular",
+    icon: require('../assets/icons/Cellular_Icon.png'),
+    page: "/settings/profile"
+  },
+  {
+    title: "Personal Hotspot",
+    icon: require('../assets/icons/Personal_Hotspot_Icon.png'),
+    page: "/settings/profile"
+  },
+  {
+    title: "Battery",
+    icon: require('../assets/icons/Battery_Icon.png'),
+    page: "/settings/profile"
+  },
+  {
+    title: "VPN",
+    icon: require('../assets/icons/VPN_Icon.png'),
+    page: "/settings/profile"
+  }
+];
